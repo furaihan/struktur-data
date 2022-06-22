e38591ab8ad0e473e27f4cab4846f9dfd6099303
furaihan <28698437+furaihan@users.noreply.github.com>
2022-06-22 10:21:22 +0700

    more experiment

--- ll.cpp
+++ ll.cpp
@@ -6,5 +6,11 @@ int main()
     MyLinkedList<int> coba;
     coba.Add(32);
     coba.Add(33);
+    printf("Int LinkedList: \n");
     coba.PrintList();
+    MyLinkedList<float> coba2;
+    coba2.Add(3.14f);
+    coba2.Add(6.62607015f);
+    printf("Float LinkedList: \n");
+    coba2.PrintList();
 }
